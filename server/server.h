ee5fe409fff3bae366d2d140695b3194ba477dbb
Ellefsen <monsoyen@gmail.com>
2024-04-16 09:28:25 +0200

    CMake initialization + gitignore with Cmake and C ignores

---
+++ server/server.h
@@ -0,0 +1,14 @@
+#ifndef __ELLEFSEN_SERVER__
+#define __ELLEFSEN_SERVER__
+#include <netinet/in.h>
+
+typedef struct EllefsenServer {
+  struct sockaddr_in serverAddress;
+
+
+
+} EllefsenServer;
+
+void init();
+
+#endif
